3952a9553aa41e30d9de99c9a9cb7578e47cccc8
igorifaresi <igor.ifaresi@gmail.com>
2020-10-15 13:50:44 -0300

    Improve http

--- http.go
+++ http.go
@@ -13,6 +13,7 @@ type HttpRequestContext struct {
 
 type HttpHandleGETFunc func(HttpRequestContext, error) (int, string)
 type HttpHandlePOSTFunc func(HttpRequestContext, []byte, error) (int, string)
+type HttpHandleWithoutErrorsPOSTFunc func(HttpRequestContext, []byte, error) (int, string)
 
 type HttpMiddlewareFunc func(http.HandlerFunc) http.HandlerFunc
 
@@ -70,6 +71,47 @@ func HttpHandlePOST(path string, handled HttpHandlePOSTFunc) {
 				statusCode, output = handled(ctx, nil, l.ErrorQueue)
 			} else {
 				statusCode, output = handled(ctx, body, nil)
+			}
+
+			w.WriteHeader(statusCode)
+			w.Write([]byte(output))
+		case "OPTIONS":
+			return
+		default:
+			w.WriteHeader(400)
+		}
+	}
+	if DefaultHttpMiddlewareFunc == nil {
+		http.HandleFunc(path, f)
+	} else {
+		http.HandleFunc(path, DefaultHttpMiddlewareFunc(f))
+	}
+}
+
+func HttpHandleWithoutErrorsPOST(path string, handled HttpHandleWithoutErrorsPOSTFunc) {
+	f := func(w http.ResponseWriter, r *http.Request) {
+		l := GetLogger()
+
+		ctx := HttpRequestContext{w, r}
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		switch r.Method {
+		case "POST":
+			var statusCode int
+			var output string
+
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				l.Error("Error in parsing body")
+				l.ErrorQueue.PrintErrors()
+
+				statusCode = 400
+				output = "Internal error"
+			} else {
+				statusCode, output = handled(ctx, body)
 			}
 
 			w.WriteHeader(statusCode)
