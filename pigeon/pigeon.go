6f5ea23ae3bc98fbd1571654a2844e09b237ef1b
Igor Fagundes [ifaresi] <48635268+igorifaresi@users.noreply.github.com>
2020-12-23 09:12:28 -0300

    Update pigeon.go

--- pigeon/pigeon.go
+++ pigeon/pigeon.go
@@ -1,5 +1,11 @@
 package pigeon
 
+import (
+	"net/http"
+	"io/ioutil"
+	"bytes"
+)
+
 type M map[string]string
 
 type Request struct {
@@ -14,6 +20,98 @@ type Response struct {
 	Text   string
 }
 
-func Send(request Request) (Response, error) {
+func SendRequestPOST(request Request) (Response, error) {
 	l := GetLogger()
+	
+	url := request.URL
+	{
+		first := true
+		for key, element := range request.Query {
+			if !first {
+				url = url+"&"
+			} else {
+				url = url+"?"	
+			}
+			url = url+key+"="+element
+		}
+	}
+	
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(request.Body))
+	if err != nil {
+		l.Error(err.Error())
+		l.Error("Unable to create http request")
+		return Response{}, l.ErrorQueue
+	}
+	
+	for key, element := range request.Header {
+		req.Header.Set(key, element)
+	}
+	
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		l.Error(err.Error())
+		l.Error("Unable to do http request")
+		return Response{}, l.ErrorQueue
+	}
+	
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		l.Error(err.Error())
+		l.Error("Unable to parse response body")
+		return Response{}, l.ErrorQueue
+	}
+	
+	return Response{
+		Status: resp.StatusCode,
+		Text: string(body),
+	}
+}
+
+func SendRequestGET(request Request) (Response, error) {
+	l := GetLogger()
+	
+	url := request.URL
+	{
+		first := true
+		for key, element := range request.Query {
+			if !first {
+				url = url+"&"
+			} else {
+				url = url+"?"	
+			}
+			url = url+key+"="+element
+		}
+	}
+	
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		l.Error(err.Error())
+		l.Error("Unable to create http request")
+		return Response{}, l.ErrorQueue
+	}
+	
+	for key, element := range request.Header {
+		req.Header.Set(key, element)
+	}
+	
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		l.Error(err.Error())
+		l.Error("Unable to do http request")
+		return Response{}, l.ErrorQueue
+	}
+	
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		l.Error(err.Error())
+		l.Error("Unable to parse response body")
+		return Response{}, l.ErrorQueue
+	}
+	
+	return Response{
+		Status: resp.StatusCode,
+		Text: string(body),
+	}
 }
