7945b1b7415c30abe189fd4ad39269885f2f45e6
Igor Fagundes [ifaresi] <48635268+igorifaresi@users.noreply.github.com>
2020-12-23 08:51:33 -0300

    Update pigeon.go

--- pigeon/pigeon.go
+++ pigeon/pigeon.go
@@ -3,17 +3,17 @@ package pigeon
 type M map[string]string
 
 type Request struct {
-  URL    string
-  Query  M
-  Header M
-  Body   []byte
+	URL    string
+	Query  M
+	Header M
+	Body   []byte
 }
 
 type Response struct {
-  Status int
-  Text   string
+	Status int
+	Text   string
 }
 
 func Send(request Request) (Response, error) {
-  l := GetLogger()
+	l := GetLogger()
 }
