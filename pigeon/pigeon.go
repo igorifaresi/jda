d3b55f1843ae571c0771a1bbe0d8e270c75f508e
Igor Fagundes [ifaresi] <48635268+igorifaresi@users.noreply.github.com>
2020-12-23 08:49:52 -0300

    Create pigeon.go

---
+++ pigeon/pigeon.go
@@ -0,0 +1,19 @@
+package pigeon
+
+type M map[string]string
+
+type Request struct {
+  URL    string
+  Query  M
+  Header M
+  Body   []byte
+}
+
+type Response struct {
+  Status int
+  Text   string
+}
+
+func Send(request Request) (Response, error) {
+  l := GetLogger()
+}
