bad4b22a1700e0ab5b749d7198a06596eb581c2d
Igor Fagundes [ifaresi] <48635268+igorifaresi@users.noreply.github.com>
2020-12-24 13:11:57 -0300

    Update pigeon.go

--- pigeon/pigeon.go
+++ pigeon/pigeon.go
@@ -9,6 +9,7 @@ import (
 type M map[string]string
 
 type Request struct {
+	Method string
 	URL    string
 	Query  M
 	Header M
@@ -20,8 +21,8 @@ type Response struct {
 	Text   string
 }
 
-func SendRequestPOST(request Request) (Response, error) {
-	l := GetLogger()
+func Send(request Request) (Response, error) {
+	l := jda.GetLogger()
 	
 	url := request.URL
 	{
@@ -36,55 +37,7 @@ func SendRequestPOST(request Request) (Response, error) {
 		}
 	}
 	
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(request.Body))
-	if err != nil {
-		l.Error(err.Error())
-		l.Error("Unable to create http request")
-		return Response{}, l.ErrorQueue
-	}
-	
-	for key, element := range request.Header {
-		req.Header.Set(key, element)
-	}
-	
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		l.Error(err.Error())
-		l.Error("Unable to do http request")
-		return Response{}, l.ErrorQueue
-	}
-	
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		l.Error(err.Error())
-		l.Error("Unable to parse response body")
-		return Response{}, l.ErrorQueue
-	}
-	
-	return Response{
-		Status: resp.StatusCode,
-		Text: string(body),
-	}
-}
-
-func SendRequestGET(request Request) (Response, error) {
-	l := GetLogger()
-	
-	url := request.URL
-	{
-		first := true
-		for key, element := range request.Query {
-			if !first {
-				url = url+"&"
-			} else {
-				url = url+"?"	
-			}
-			url = url+key+"="+element
-		}
-	}
-	
-	req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest(request.Method, url, bytes.NewBuffer(request.Body))
 	if err != nil {
 		l.Error(err.Error())
 		l.Error("Unable to create http request")
